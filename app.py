4ad6f09f0e26ab15cb9f0940240fd4856f1ae5db
mling <sapient007@hotmail.com>
2018-01-15 22:46:51 -0500

    made changes to make it CF ready

--- app.py
+++ app.py
@@ -3,28 +3,29 @@ from flask_restful import Resource, Api, abort
 from flask_restful import reqparse
 from flask_jsonpify import jsonify
 from json import dumps
+from datetime import datetime
 from datetime import timedelta
 from dateutil.parser import parse
 from docopt import docopt
 from geopy import geocoders
 from math import trunc
 from tzlocal import get_localzone
-import datetime
 import pytz
 import requests
 import sys
 import time
+import logging
 
 app = Flask(__name__)
 api = Api(app)
 
-API_KEY = 'l7xxb3dcccc4a5674bada48fc6fcf0946bc8'
+#API_KEY = 'l7xxb3dcccc4a5674bada48fc6fcf0946bc8'
+API_KEY = 'l7xx12ebcbc825eb480faa276e7f192d98d1'
 USER_EXPERIENCE_KEY = 'AAAA3198-4545-46F4-9A05-BB3E868BEFF5'
 BASE_URL = 'https://mobile.southwest.com/api/'
 CHECKIN_EARLY_SECONDS = 5
 CHECKIN_INTERVAL_SECONDS = 0.25
-MAX_ATTEMPTS = 10
-
+MAX_ATTEMPTS = 1
 
 parser = reqparse.RequestParser()
 parser.add_argument('reservation_id')
@@ -32,19 +33,91 @@ parser.add_argument('first_name')
 parser.add_argument('last_name')
 
 reservations = {
-    '1': {
+    '111111': {
         'first_name': 'ming',
         'last_name': 'ling',
         'status': 'pending',
-        'reservation_date': datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d_%h')
+        'legs': [
+            {
+                'order': '1',
+                'airport_code': 'DCA',
+                'status': 'pending',
+                'seat': '',
+                'flight_time_local': datetime.strftime(datetime.utcnow(), '%Y-%m-%d %H:%M'),
+                'flight_time_utc': datetime.strftime(datetime.utcnow(), '%Y-%m-%d %H:%M')
+            },
+            {
+                'order': '2',
+                'airport_code': 'BWI',
+                'status': 'pending',
+                'seat': '',
+                'flight_time_local': datetime.strftime(datetime.utcnow(), '%Y-%m-%d %H:%M'),
+                'flight_time_utc': datetime.strftime(datetime.utcnow(), '%Y-%m-%d %H:%M')
+            }
+        ]
     },
-    '2': {
+    'AAAAAA': {
         'first_name': 'ming',
         'last_name': 'ling',
         'status': 'pending',
-        'reservation_date': datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d_%h')
+        'legs': [
+            {
+                'order': '1',
+                'airport_code': 'IAD',
+                'status': 'pending',
+                'seat': '',
+                'flight_time_local': datetime.strftime(datetime.utcnow(), '%Y-%m-%d %H:%M'),
+                'flight_time_utc': datetime.strftime(datetime.utcnow(), '%Y-%m-%d %H:%M')
+            },
+            {
+                'order': '2',
+                'airport_code': 'PDX',
+                'status': 'pending',
+                'seat': '',
+                'flight_time_local': datetime.strftime(datetime.utcnow(), '%Y-%m-%d %H:%M'),
+                'flight_time_utc': datetime.strftime(datetime.utcnow(), '%Y-%m-%d %H:%M')
+            }
+        ]
+    },
+    'TKJPZC': {
+        "first_name": "ming",
+        "last_name": "ling",
+        "legs": [
+            {
+                "airport_code": "Washington (Reagan National), DC",
+                "flight_time_local": "2018-02-17 06:25",
+                "flight_time_utc": "2018-02-17 11:25",
+                "order": 1,
+                "status": "pending"
+            },
+            {
+                "airport_code": "Portland, OR",
+                "flight_time_local": "2018-02-20 07:00",
+                "flight_time_utc": "2018-02-20 15:00",
+                "order": 2,
+                "status": "pending"
+            },
+            {
+                "airport_code": "San Diego, CA",
+                "flight_time_local": "2018-02-23 14:40",
+                "flight_time_utc": "2018-02-23 22:40",
+                "order": 3,
+                "status": "pending"
+            }
+        ],
+        "status": "pending"
     }
 }
+
+logger = logging.getLogger('SWCheckin')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+utc = pytz.timezone('UTC')
+
 
 def abort_if_invalid_reservation_id(reservation_id):
     if reservation_id not in reservations:
@@ -52,6 +125,7 @@ def abort_if_invalid_reservation_id(reservation_id):
 
 # Pulled from proxying the Southwest iOS App
 headers = {'Host': 'mobile.southwest.com', 'Content-Type': 'application/json', 'X-API-Key': API_KEY, 'X-User-Experience-Id': USER_EXPERIENCE_KEY, 'Accept': '*/*'}
+
 
 # You might ask yourself, "Why the hell does this exist?"
 # Basically, there sometimes appears a "hiccup" in Southwest where things
@@ -64,12 +138,13 @@ def safe_request(url, body=None):
         else:
             r = requests.get(url, headers=headers)
         data = r.json()
-        import ipdb; ipdb.set_trace()
+        #import ipdb; ipdb.set_trace()
         if 'httpStatusCode' in data and data['httpStatusCode'] in ['NOT_FOUND', 'BAD_REQUEST', 'FORBIDDEN']:
             attempts += 1
-            print("StatusCode:" + data['httpStatusCode'] + ", Message: " + data['message'])
+            logger.debug("StatusCode:" + data['httpStatusCode'] + ", Message: " + data['message'])
             if attempts > MAX_ATTEMPTS:
-                sys.exit("Unable to get data, killing self")
+                #sys.exit("Unable to get data, killing self")
+                return body
             time.sleep(CHECKIN_INTERVAL_SECONDS)
             continue
         return data
@@ -78,6 +153,8 @@ def lookup_existing_reservation(number, first, last):
     # Find our existing record
     url = "{}mobile-misc/v1/mobile-misc/page/view-reservation/{}?first-name={}&last-name={}".format(BASE_URL, number, first, last)
     data = safe_request(url)
+    if data is None:
+        return data
     return data['viewReservationViewPage']
 
 def get_checkin_data(number, first, last):
@@ -89,7 +166,7 @@ def checkin(number, first, last):
     data = get_checkin_data(number, first, last)
     info_needed = data['_links']['checkIn']
     url = "{}mobile-air-operations{}".format(BASE_URL, info_needed['href'])
-    print("Attempting check-in...")
+    logger.debug("Attempting check-in...")
     return safe_request(url, info_needed['body'])['checkInConfirmationPage']
 
 def schedule_checkin(flight_time, number, first, last):
@@ -102,36 +179,50 @@ def schedule_checkin(flight_time, number, first, last):
         # pretty print our wait time
         m, s = divmod(delta, 60)
         h, m = divmod(m, 60)
-        print("Too early to check in.  Waiting {} hours, {} minutes, {} seconds".format(trunc(h), trunc(m), s))
+        logger.debug("Too early to check in.  Waiting {} hours, {} minutes, {} seconds".format(trunc(h), trunc(m), s))
         time.sleep(delta)
     data = checkin(number, first, last)
     for flight in data['flights']:
         for doc in flight['passengers']:
-            print("{} got {}{}!".format(doc['name'], doc['boardingGroup'], doc['boardingPosition']))
+            logger.debug("{} got {}{}!".format(doc['name'], doc['boardingGroup'], doc['boardingPosition']))
 
 def auto_checkin(reservation_number, first_name, last_name):
     body = lookup_existing_reservation(reservation_number, first_name, last_name)
 
+    if body is None:
+        return -1
+    logger.debug("legs found, adding to record")
     # setup a geocoder
     # needed since Southwest no longer includes TZ information in reservations
     g = geocoders.GoogleV3()
 
     # Get our local current time
+    #import ipdb; ipdb.set_trace()
     now = datetime.now(pytz.utc).astimezone(get_localzone())
     tomorrow = now + timedelta(days=1)
 
     # find all eligible legs for checkin
+    reservations[reservation_number] = {'first_name': first_name, 'last_name': last_name, 'status': 'pending', 'legs': []}
+    legcounter = 0
     for leg in body['bounds']:
         # calculate departure for this leg
+        legcounter += 1
         airport = "{}, {}".format(leg['departureAirport']['name'], leg['departureAirport']['state'])
         takeoff = "{} {}".format(leg['departureDate'], leg['departureTime'])
         point = g.geocode(airport).point
+        logger.debug("airport: %s, take off: %s, point: %s", airport, takeoff, point)
+
         airport_tz = g.timezone(point)
         date = airport_tz.localize(datetime.strptime(takeoff, '%Y-%m-%d %H:%M'))
         if date > now:
             # found a flight for checkin!
-            print("Flight information found, departing {} at {}".format(airport, date.strftime('%b %d %I:%M%p')))
-            schedule_checkin(date, reservation_number, first_name, last_name)
+            logger.debug("Flight information found, departing {} at {}".format(airport, date.strftime('%Y-%m-%d %H:%M')))
+            #add pending and datetime to reservation
+            reservations[reservation_number]['legs'].append({'order': legcounter, 'airport_code': airport, 'flight_time_local':  datetime.strftime(date, '%Y-%m-%d %H:%M'),
+            'flight_time_utc': datetime.strftime(date.astimezone(utc), '%Y-%m-%d %H:%M') , 'status': 'pending'})
+
+            #leverage scheduler in PCF to schedule this reservation
+            #schedule_checkin(date, reservation_number, first_name, last_name)
 
 
 class Reservation(Resource):
@@ -150,10 +241,36 @@ class Reservations(Resource):
     def get(self):
         return reservations
 
-# api.add_resource(Checkin, '/reservation/')
-api.add_resource(Reservation, '/reservation/<reservation_id>')
-api.add_resource(Reservations, '/reservations')
+    def post(self):
+        logger.debug("Posting")
+        args = parser.parse_args()
+        reservation_id = args['reservation_id']
+        if len(reservation_id) is not 6:
+            return "invalid reservation number", 201
+        first_name = args['first_name']
+        last_name = args['last_name']
+        if reservation_id in reservations:
+            return "reservation is already in queue", 201
+        response = auto_checkin(reservation_id, first_name, last_name )
+        if response is -1:
+            return "reservation code appears to be invalid", 404
+        #reservations[reservation_id] = {'first_name': first_name, 'last_name': last_name}
+        return reservations[reservation_id], 201
+
+class Checkin(Resource):
+    """takes on a confirmation number for checkin and can also del a confirmation"""
+    def get(self, reservation_id):
+        abort_if_invalid_reservation_id(reservation_id)
+        data = checkin(reservations[reservation_id], reservations[reservation_id].get("first_name"), reservations[reservation_id].get("last_name") )
+        for flight in data['flights']:
+            for doc in flight['passengers']:
+                logger.info("{} got {}{}!".format(doc['name'], doc['boardingGroup'], doc['boardingPosition']))
+        return reservations[reservation_id], 201
+
+api.add_resource(Reservation, '/reservations/<reservation_id>')
+api.add_resource(Reservations, '/reservations/')
+api.add_resource(Checkin, '/checkin/<reservation_id>')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
